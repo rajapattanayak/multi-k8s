4ca1f45dd102d1d9e9e4b9bcfce36e78277fedc1
rajapattanayak <pattanayak.raja@gmail.com>
2019-05-10 18:04:35 +0530

    Added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t praja/multi-client:latest -t praja/multi-client:$SHA ./client/Dockerfile ./client
+docker build -t praja/multi-server:latest -t praja/multi-server:$SHA ./server/Dockerfile ./server
+docker build -t praja/multi-worker:latest -t praja/multi-worker:$SHA ./worker/Dockerfile ./worker
+
+docker push praja/multi-client:latest
+docker push praja/multi-server:latest
+docker push praja/multi-worker:latest
+
+docker push praja/multi-client:$SHA
+docker push praja/multi-server:$SHA
+docker push praja/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=praja/multi-client:$SHA
+kubectl set image deployments/server-deployment server=praja/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=praja/multi-worker:$SHA
